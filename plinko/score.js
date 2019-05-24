c53714b5f41c502994c2ca4daad53dccfc3ae67f
Jacky Lui <jackymlui@live.com>
2019-05-25 09:39:20 +1000

    forked repo and up to testing accuracy of knn model

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,50 @@
+const outputs = [];
+// const predictionPoint = 300;
+const k = 15;
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+  // console.log(outputs);
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
+  const testSetSize = 10;
+  const [testSet, trainingSet] = splitDataset(outputs, testSetSize); // 2015 destructuring syntax
+  let numberCorrect = 0;
+  for (let i = 0; i < testSet.length; i++) {
+    const bucket = knn(trainingSet, testSet[i][0]);
+    // console.log(bucket, testSet[i][3]);
+    if (bucket === testSet[i][3]) numberCorrect++;
+  }
+  // console.log('Your point will probably fall into', bucket);
+  console.log("Accuracy: " + numberCorrect / testSetSize);
+
+  
 }
 
+function knn(data, point) {
+  return _.chain(data)
+    .map(row => [distance(row[0], point), row[3]])
+    .sortBy(row => row[0])
+    .slice(0, k)
+    .countBy(row => row[1])
+    .toPairs()
+    .sortBy(row => row[1])
+    .last()
+    .first()
+    .parseInt()
+    .value();
+}
+
+function distance(pointA, pointB) {
+  return Math.abs(pointA - pointB);
+}
+
+function splitDataset(data, testCount) {
+  const shuffled = _.shuffle(data);
+  const testSet = _.slice(shuffled, 0, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
+
+  return [testSet, trainingSet];
+}
