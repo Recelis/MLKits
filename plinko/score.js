a79d5d7526e62adf0a98f71878e95fa8f45e0f3c
Jacky Lui <jackymlui@live.com>
2019-05-30 09:39:42 +1000

    modifying analysis for multple features

--- plinko/score.js
+++ plinko/score.js
@@ -12,7 +12,7 @@ function runAnalysis() {
   const [testSet, trainingSet] = splitDataset(outputs, testSetSize); // 2015 destructuring syntax
   _.range(1, 20).forEach(k => {
     const accuracy = _.chain(testSet)
-      .filter(testPoint => knn(trainingSet, testPoint[0], k) === testPoint[3])
+      .filter(testPoint => knn(trainingSet, _.initial(testPoint), k) === testPoint[3]) // initial to remove label off testpoint before getting into knn
       .size() // basically length
       .divide(testSetSize)
       .value();
@@ -22,7 +22,12 @@ function runAnalysis() {
 
 function knn(data, point, k) {
   return _.chain(data) // allows you to chain methods together on an array
-    .map(row => [distance(row[0], point), row[3]]) // distance from dropPosition to the testPoint. Returns [diffdistance]
+    // .map(row => [distance(row[0], point), row[3]]) // distance from dropPosition to the testPoint. Returns [diffdistance]
+     .map((row => { // each row
+       return [distance(_.initial(row), point), // don't want to remove label off here on point, because it will be annoying to do this if manually input point.
+        _.last(row)
+      ];
+     })) 
     .sortBy(row => row[0]) // sort by [diffdistance]
     .slice(0, k) // arbitrary take only top 15
     .countBy(row => row[1]) //
@@ -35,7 +40,13 @@ function knn(data, point, k) {
 }
 
 function distance(pointA, pointB) {
-  return Math.abs(pointA - pointB);
+  // assume pointA is an array and pointB is same.
+  // return Math.abs(pointA - pointB);
+  return _.chain(pointA)
+	.zip(pointB)
+	.map(([a,b]) => (a-b)**2)
+	.sum()
+	.value() **0.5;
 }
 
 function splitDataset(data, testCount) {
